4d37e3cb93a9b4fa7f6f46035cefe420649b5f42
Rahul Kumar Pandey <rahulpandey1630@gmail.com>
2024-09-20 13:51:53 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-circom";
 // circuits
 import circuits = require('./circuits.config.json')
-
+import 'dotenv/config';
 // set env var to the root of the project
 process.env.BASE_PATH = __dirname;
 
@@ -22,8 +22,15 @@ const config: HardhatUserConfig = {
       }
     ]
   },
+  networks:{
+    hardhat: {},
+    sepolia: {
+      url: 'https://ethereum-sepolia-rpc.publicnode.com',
+      accounts: [process.env.MyKey]
+    }
+  },
   circom: {
-    // (optional) Base path for input files, defaults to `./circuits/`
+    // (optional) Base path for input files, defaults to ./circuits/
     inputBasePath: "./circuits",
     // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
     ptau: "powersOfTau28_hez_final_12.ptau",
@@ -32,4 +39,4 @@ const config: HardhatUserConfig = {
   },
 };
 
-export default config;
+export default config;
